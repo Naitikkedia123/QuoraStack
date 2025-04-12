ac52275418dcb5bcab0f7ca9c7d83a2a9e5f9851
Naitikkedia123 <kedianaitik2006@gmail.com>
2025-04-12 16:35:05 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose= require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+const userSchema = new Schema({
+    email: { type: String, required: true, unique: true },
+    highscore: { type: Number, default: 0 },
+});
+
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', userSchema);
